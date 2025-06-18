6fc2cd421e1a79b7379902059bcbe2caf6547679
fabian beleño <125183140+fabianbele2605@users.noreply.github.com>
2025-06-18 13:02:33 -0500

    Add files via upload

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -3,12 +3,14 @@ console.log("¡Bienvenido al sistema interactivo de fabian!")
 
 // vamos a ingresar los datos
 
-let nombre = prompt("Por favor,ingresa tu nombre:");
-if (nombre !== null) {
-    alert(`Hola, ${nombre}!`);
-} else {
-    alert('No ingresate tu nombre.')
+let ingresarNombre = "";
+while 
+(ingresarNombre === "" || ingresarNombre === null) {
+  ingresarNombre = prompt("Por favor, ingresa tu nombre:");
 }
+
+alert("Hola, " + ingresarNombre);
+
 
 let edad = prompt("Por favor, ingresa tu edad:")
 
@@ -19,10 +21,9 @@ edad = parseInt(edad);
 // validacion y mensaje 
 if (isNaN(edad)) {
     console.error("Error, por favor ingresa una edad valida numerica.");
-} else 
-if (edad => 18) {
-    alert(`Hola, ${nombre} eres mayor de edad🧔, sigue codificando y preparate para ser de los mejores en la programacion🔥💻`)
+} else if (edad > 17) {
+    alert(`Hola, ${ingresarNombre} eres mayor de edad🧔, sigue codificando y preparate para ser de los mejores en la programacion🔥💻`)
 } else {
-    alert(`Hola, ${nombre} eres menor de edad🧒, pero puedes seguir codificando y aprendiendo mucho mas 💻⚡`)
+    alert(`Hola, ${ingresarNombre} eres menor de edad🧒, pero puedes seguir codificando y aprendiendo mucho mas 💻⚡`)
 }
 
