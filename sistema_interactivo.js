19e67f77ffab31a826377f9a9af4da8eb5e1745e
fabian beleño <125183140+fabianbele2605@users.noreply.github.com>
2025-06-20 07:29:50 -0500

    Add files via upload

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -1,29 +1,33 @@
 // Inicializacion de programa
-console.log("¡Bienvenido al sistema interactivo de fabian!")
+console.log("Bienvenido al sistema interactivo de fabian!")
 
 // vamos a ingresar los datos
-
+let continuar = true;
 let ingresarNombre = "";
-while 
-(ingresarNombre === "" || ingresarNombre === null) {
-  ingresarNombre = prompt("Por favor, ingresa tu nombre:");
+
+// ciclo while para validar que no deje vacio el prompt.
+
+while (ingresarNombre === "" || ingresarNombre === null) {
+    ingresarNombre = prompt("Por favor, ingresa tu nombre:");
+    if (ingresarNombre === null) {
+        alert("Operación cancelada.");
+        continuar = false;
+        break; 
+    }
 }
 
-alert("Hola, " + ingresarNombre);
+if (continuar) {
+    let edad = prompt("Por favor, ingresa tu edad:");
 
+    // convertir edad a numero
+    edad = parseInt(edad);
 
-let edad = prompt("Por favor, ingresa tu edad:")
-
-// convertir edad a numero
-
-edad = parseInt(edad);
-
-// validacion y mensaje 
-if (isNaN(edad)) {
-    console.error("Error, por favor ingresa una edad valida numerica.");
-} else if (edad > 17) {
-    alert(`Hola, ${ingresarNombre} eres mayor de edad🧔, sigue codificando y preparate para ser de los mejores en la programacion🔥💻`)
-} else {
-    alert(`Hola, ${ingresarNombre} eres menor de edad🧒, pero puedes seguir codificando y aprendiendo mucho mas 💻⚡`)
+    // validacion y mensaje 
+    if (isNaN(edad)) {
+        console.error("Error, por favor ingresa una edad valida numerica.");
+    } else if (edad > 17) {
+        alert(`Hola, ${ingresarNombre} eres mayor de edad🧔, sigue codificando y preparate para ser de los mejores en la programacion🔥💻`);
+    } else {
+        alert(`Hola, ${ingresarNombre} eres menor de edad🧒, pero puedes seguir codificando y aprendiendo mucho mas 💻⚡`);
+    }
 }
-
